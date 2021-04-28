3bc191b3336dd3e9ab3e02d8a7534d53d87599ea
dusarah <71254323+dusarah@users.noreply.github.com>
2021-04-28 16:28:39 +0200

    Update settings.py

--- Awwwward/Awwwward/settings.py
+++ Awwwward/Awwwward/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
+
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -73,14 +74,13 @@ WSGI_APPLICATION = 'Awwwward.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
-
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
 
